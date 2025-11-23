1ec56e9374dcc7d0d2751614827cb28806b8f65e
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-23 09:26:47 +0300

    update index.tsx and globals.css

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,14 +1,9 @@
-import Layout from "@/components/layouts/Layout";
-
-export default function Home() {
+const Home = () => {
   return (
-    <Layout>
-      <div className="text-center py-20">
-        <h1 className="text-4xl font-bold">Welcome to Splash App</h1>
-        <p className="text-gray-600 mt-4">
-          Modern Next.js layout using the DRY principle.
-        </p>
-      </div>
-    </Layout>
+    <div>
+      <h1>Welcome to Splash App</h1>
+    </div>
   );
-}
+};
+
+export default Home;
